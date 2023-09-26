e6c0ba9b450caecddcb72aafafcf31a97b1a2f3f
camilo Plaza <C0526035@camosun.ca>
2023-09-26 14:25:30 -0700

    progress 1

--- ICS214Lab2/Source.cpp
+++ ICS214Lab2/Source.cpp
@@ -37,6 +37,33 @@ int getIntFromUserInRange(int min, int max) {
     }
 }
 
+int promptForYear();
+// prompt the user for the year and return the result
+
+int promptForMonth();
+// prompt the user for the month and return the result
+
+int promptForDayOfMonth();
+// prompt the user for the day of the month (0-31) and return the result
+
+Weekday calculateWeekday(int month, int dayOfMonth, int year);
+// use Zeller’s congrunce algorithm to calculate and return a Weekday
+
+std::string getWeekdayName(Weekday day);
+
+// There isn’t a straightforward way of printing out the text representation of an
+// enumerated type,so we’ll write our own
+// Given a Weekday, return a string to represent its name.
+// You can do this in several ways:
+// 1. multiple if/else if statements
+// 2. a switch statement
+// 3. create an array of string literals, and index the array by
+// doing a static_cast on the Weekday to get its int representation.
+//The third option is the best(most elegant / extensible).
+//To do this, you need to know how to create an array of string objects(see
+//   below) :
+//eg: std::string myArray[] = { "a", "b", “c” }; .
+
 int main() {
 
 
